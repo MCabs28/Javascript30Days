f9194f00ebff2dfc04bd539530bdb397c5f3096b
Mcabs28 <markcabael.official@gmail.com>
2023-06-28 20:02:41 +0800

    Done Day 8 HTML Canvas

--- Day 8 HTML Canvas/index.js
+++ Day 8 HTML Canvas/index.js
@@ -10,15 +10,22 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 100;
+ctx.lineCap = 100;
+
+// blend the  color
+ctx.globalCompositeOperation = 'multiply';
+
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e){
     if(!isDrawing) return; // stop to fire from running when they are not moused down
     console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
     
     // start from
@@ -27,6 +34,23 @@ function draw(e){
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();    
     [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+
+    if(hue >= 360){
+        hue = 0
+    }
+
+    if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1){
+        direction = !direction;
+    }
+
+    if(direction){
+        ctx.lineWidth++;
+    }else{
+        ctx.lineWidth--;
+    }
+
+    
 }
 
 canvas.addEventListener('mousedown', (e) => {
